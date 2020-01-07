2c2d38a38780e1feb8ec8c774b1ababad5dd1385
Alejandro Mendoza <zippi180@gmail.com>
2020-01-07 00:03:14 -0600

    Created Constants file

---
+++ todo/Model/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  todo
+//
+//  Created by Alejandro Mendoza on 06/01/20.
+//  Copyright © 2020 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+let CornerRadius: CGFloat = 10
