8aa33af9f25cf865b32d74bf5c6be618775223a6
Alejandro Mendoza <zippi180@gmail.com>
2020-01-07 23:10:15 -0600

    Added basic layout for a view to add a todo

---
+++ todo/View/Todo/New/AddTodo.swift
@@ -0,0 +1,16 @@
+//
+//  AddTodo.swift
+//  todo
+//
+//  Created by Alejandro Mendoza on 07/01/20.
+//  Copyright © 2020 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class AddTodo: UIView {
+
+    @IBOutlet var ContainerView: AddTodo!
+    
+
+}
