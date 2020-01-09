ea7ae9bd33ec765f10c7192bd38dab87f3a6b258
Alejandro Mendoza <zippi180@gmail.com>
2020-01-08 23:41:31 -0600

    Implemented basic connections for 'Add a Todo'

---
+++ todo/Controller/TodosDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TodosDetailViewController.swift
+//  todo
+//
+//  Created by Alejandro Mendoza on 07/01/20.
+//  Copyright © 2020 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class TodosDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- todo/View/Todo/New/AddTodo.swift
+++ todo/View/Todo/New/AddTodo.swift
@@ -7,10 +7,38 @@
 //
 
 import UIKit
-
+@IBDesignable
 class AddTodo: UIView {
 
-    @IBOutlet var ContainerView: AddTodo!
+    @IBOutlet var containerView: AddTodo!
+    @IBOutlet weak var todoBackgroundView: UIView!
     
-
+    @IBOutlet weak var todoTextView: UITextView!
+    
+    
+    override init(frame: CGRect) {
+       super.init(frame: frame)
+       customInit()
+    }
+       
+    required init?(coder: NSCoder) {
+       super.init(coder: coder)
+       customInit()
+    }
+    
+    private func customInit(){
+        let bundle = Bundle(for: AddTodo.self)
+        bundle.loadNibNamed(String(describing: AddTodo.self), owner: self, options: nil)
+        addSubview(containerView)
+        containerView.frame = bounds
+        containerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        alignTextVerticallyInContainer()
+    }
+    
+    func alignTextVerticallyInContainer() {
+    var topCorrect = (todoTextView.bounds.size.height - todoTextView.contentSize.height * todoTextView.zoomScale) / 2
+            topCorrect = topCorrect < 0.0 ? 0.0 : topCorrect;
+    todoTextView.contentInset.top = topCorrect
+        }
+    
 }
