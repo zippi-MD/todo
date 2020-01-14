517e8b0d3b8ed832e0ff5a053317a13cd66136ec
Alejandro Mendoza <zippi180@gmail.com>
2020-01-13 23:31:44 -0600

    Implemented Toolbar View

---
+++ todo/View/Todo/Toolbar/Toolbar.swift
@@ -0,0 +1,80 @@
+//
+//  Toolbar.swift
+//  todo
+//
+//  Created by Alejandro Mendoza on 12/01/20.
+//  Copyright © 2020 Alejandro Mendoza. All rights reserved.
+//
+
+import UIKit
+
+protocol ToolbarDelegate: class {
+    func addActionWasSelected()
+    func scheduleActionWasSelected()
+    func discardActionWasSelected()
+}
+
+@IBDesignable
+class Toolbar: UIView {
+
+    @IBOutlet var containerView: UIView!
+    @IBOutlet weak var addBackgroundView: UIView!
+    @IBOutlet weak var scheduleBackgroundView: UIView!
+    @IBOutlet weak var discardBackgorundView: UIView!
+    
+    let actionsCornerRadius: CGFloat = 7.0
+    
+    weak var delegate: ToolbarDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        customInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        customInit()
+    }
+    
+    func customInit(){
+        let bundle = Bundle(for: AddTodo.self)
+        bundle.loadNibNamed(String(describing: Toolbar.self), owner: self, options: nil)
+        addSubview(containerView)
+        containerView.frame = bounds
+        containerView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        setupActionsCornerRadius()
+        addGestureRecognizerToActions()
+    }
+    
+    func setupActionsCornerRadius(){
+        addBackgroundView.layer.cornerRadius = actionsCornerRadius
+        scheduleBackgroundView.layer.cornerRadius = actionsCornerRadius
+        discardBackgorundView.layer.cornerRadius = actionsCornerRadius
+    }
+    
+    func addGestureRecognizerToActions(){
+        let addTap = UITapGestureRecognizer(target: self, action: #selector(addActionSelected))
+        addBackgroundView.addGestureRecognizer(addTap)
+        
+        let scheduleTap = UITapGestureRecognizer(target: self, action: #selector(scheduleActionSelected))
+        scheduleBackgroundView.addGestureRecognizer(scheduleTap)
+        
+        let discardTap = UITapGestureRecognizer(target: self, action: #selector(discardActionSelected))
+        discardBackgorundView.addGestureRecognizer(discardTap)
+    }
+
+}
+
+// MARK: Call to delegate
+extension Toolbar {
+    @objc func addActionSelected(){
+        delegate?.addActionWasSelected()
+    }
+    @objc func scheduleActionSelected(){
+        delegate?.scheduleActionWasSelected()
+    }
+    @objc func discardActionSelected(){
+        delegate?.discardActionWasSelected()
+    }
+}
