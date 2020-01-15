b90da8c4c2c8fc7ff82eab526afe5e821690c814
Alejandro Mendoza <zippi180@gmail.com>
2020-01-14 23:04:34 -0600

    Implemented functionality to handle more keyboard events

--- todo/Controller/TodosDetailViewController.swift
+++ todo/Controller/TodosDetailViewController.swift
@@ -11,15 +11,21 @@ import UIKit
 class TodosDetailViewController: UIViewController {
 
     
+    @IBOutlet weak var addTodoView: AddTodo!
     @IBOutlet weak var toolbarView: Toolbar!
     @IBOutlet weak var toolbarBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var addTodoBottomConstraint: NSLayoutConstraint!
     
     let keyboardHandler = KeyboardEvents()
+    
+    var actualTodoDetailState: TodoState = .discard
+    var actualKeyboardState: KeyboardState = .hidden
     
     override func viewDidLoad() {
         super.viewDidLoad()
         keyboardHandler.registerForKeyboardEvents()
         keyboardHandler.delegate = self
+        toolbarView.delegate = self
     }
     
     
@@ -27,7 +33,21 @@ class TodosDetailViewController: UIViewController {
         super.viewWillDisappear(animated)
         keyboardHandler.unregisterFromKeyboardEvents()
     }
-
+    @IBAction func activateAddTodo(_ sender: Any) {
+        actualTodoDetailState = .present
+        let animationDuration: TimeInterval = 0.25
+        addTodoView.alpha = 0
+        addTodoView.isHidden = false
+        
+        UIView.animate(withDuration: animationDuration, animations: {
+            self.addTodoView.alpha = 1
+            self.addTodoBottomConstraint.constant = self.view.frame.height * 0.4
+            self.view.layoutIfNeeded()
+        }) { (_) in
+            self.addTodoView.todoTextView.becomeFirstResponder()
+        }
+    }
+    
 }
 
 // MARK: - Handle Keyboard Events
@@ -54,7 +74,23 @@ extension TodosDetailViewController: KeyboardHandlingDelegate {
     }
     
     func keyboardWillHide(_ notification: Notification) {
+        if actualTodoDetailState == .discard {
+            UIView.animate(withDuration: 0.25) {
+                self.toolbarView.alpha = 0
+                self.toolbarBottomConstraint.constant = 0
+                self.view.layoutIfNeeded()
+            }
+        }
         
+        else if actualTodoDetailState == .schedule {
+            let animationDuration = notification.keyboardAnimationDuration ?? 0.25
+            UIView.animate(withDuration: animationDuration, animations: {
+                self.toolbarView.alpha = 0
+                self.toolbarBottomConstraint.constant = 0
+            }) { (_) in
+                 
+            }
+        }
     }
     
     func keyboardDidHide(_ notification: Notification) {
@@ -71,3 +107,56 @@ extension TodosDetailViewController: KeyboardHandlingDelegate {
     
     
 }
+
+// MARK: - Handle Toolbar Events
+extension TodosDetailViewController: ToolbarDelegate {
+    func addActionWasSelected() {
+
+    }
+    
+    func scheduleActionWasSelected() {
+        actualTodoDetailState = .schedule
+        
+        addTodoView.todoTextView.resignFirstResponder()
+    }
+    
+    func discardActionWasSelected() {
+        
+        actualTodoDetailState = .discard
+        let animationDuration: TimeInterval = 0.25
+        
+        UIView.animate(withDuration: animationDuration, animations: {
+            self.addTodoBottomConstraint.constant = 0
+            self.addTodoView.alpha = 0
+            self.view.layoutIfNeeded()
+        }) { (_) in
+            self.addTodoView.todoTextView.resignFirstResponder()
+            self.addTodoView.isHidden = true
+        }
+        
+    }
+    
+    
+}
+
+// MARK: - Handle View Updates
+extension TodosDetailViewController {
+    
+    enum TodoState {
+        case present
+        case keyboardHidden
+        case discard
+        case schedule
+    }
+    
+    enum KeyboardState{
+        case hidden
+        case presented
+    }
+    
+}
+
+
+
+
+
