bb8e8c10b69a6d4f3e7d0ce15fef38c5e497253a
Alejandro Mendoza <zippi180@gmail.com>
2020-01-21 15:01:58 -0600

    Added constants

--- todo/Model/Constants.swift
+++ todo/Model/Constants.swift
@@ -11,4 +11,6 @@ import UIKit
 struct Constants {
     static let TodoCornerRadius: CGFloat = 4.0
     static let ToolbarActionBorderWidth: CGFloat = 0.5
+    static let DetailTodoCellIdentifier: String = "TodoCell"
+    static let TagIdentifier: String = "#"
 }
